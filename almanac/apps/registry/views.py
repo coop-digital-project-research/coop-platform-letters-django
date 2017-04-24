8c86d90b93144076e29f564463aa493fd1c7d772
Paul M Furley <paul@paulfurley.com>
2017-04-24 15:24:31 +0100

    Add basic template, view, url for CommunityEnergyGroup

--- almanac/apps/registry/views.py
+++ almanac/apps/registry/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import CommunityEnergyGroup
+
+
+class CommunityEnergyGroupDetail(DetailView):
+    template_name = 'registry/community_energy_group_detail.html'
+    model = CommunityEnergyGroup
+    context_object_name = 'group'
