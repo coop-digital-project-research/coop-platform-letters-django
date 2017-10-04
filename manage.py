42329fe19c31e4af74f27e357cb0127b138605f6
Paul M Furley <paulfurley@users.noreply.github.com>
2017-10-04 12:21:14 +0100

    Minor: set default django settings in manage.py (#36)
    This is what's used if it's not set by the environment.

--- manage.py
+++ manage.py
@@ -3,7 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "letters.settings")
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        "letters.settings.production"
+    )
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
