917025debdeb8588f8ae806f8755683e30f93738
Paul M Furley <paul@paulfurley.com>
2017-10-10 14:12:15 +0100

    Add WriterReaderAllocation representing a 'match'
    This database model indicates a letter that should be written from a
    writer to a reader.
    
    It uses a random reference like '123-456' which passes the 'luhn check'
    - the final digit can check the validity of the 5 random ones.

---
+++ letters/apps/matchmaker/utils.py
@@ -0,0 +1,26 @@
+import luhn
+import random
+
+
+def generate_reference():
+    """
+    Return a luhn-valid 10-digit reference like:
+    123-456  (where 6 is the check digit)
+    """
+    from .models import WriterReaderAllocation
+
+    characters = '0123456789'
+
+    for attempt in range(10):
+        five_digits = ''.join(random.choice(characters) for _ in range(5))
+        reference = '{}-{}{}'.format(
+            five_digits[0:3],
+            five_digits[3:5],
+            luhn.generate(five_digits))
+
+        try:
+            WriterReaderAllocation.objects.get(reference=reference)
+        except WriterReaderAllocation.DoesNotExist:
+            return reference
+
+    raise RuntimeError('Failed repeatedly to generate a unique reference.')
