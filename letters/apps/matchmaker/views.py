87db1c7ca90bf109d335c2f9a24e79e94866729f
Paul M Furley <paul@paulfurley.com>
2017-10-17 16:48:31 +0100

    Add task to delete unresponsive writers

--- letters/apps/matchmaker/views.py
+++ letters/apps/matchmaker/views.py
@@ -207,6 +207,9 @@ class AdminTaskListView(TemplateView):
             'writers_awaiting_final_chase_email':
             self._writers_awaiting_final_chase_email(),
 
+            'writers_unresponsive':
+            self._writers_unresponsive(),
+
             'readers_awaiting_baseline_survey_email':
             self._readers_awaiting_baseline_survey_email(),
 
@@ -261,6 +264,14 @@ class AdminTaskListView(TemplateView):
             age=None,
             chase_email_sent__lt=three_days_ago,
         ).order_by('chase_email_sent')
+
+    def _writers_unresponsive(self):
+        seven_days_ago = timezone.now() - datetime.timedelta(days=7)
+
+        return Writer.objects.filter(
+            age=None,
+            final_chase_email_sent__lt=seven_days_ago,
+        ).order_by('final_chase_email_sent')
 
     def _readers_awaiting_baseline_survey_email(self):
         return Reader.objects.filter(
